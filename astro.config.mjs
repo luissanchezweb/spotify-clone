db64e602c13d08d22648274679be881e2cebe961
Luis Sánchez <luissanchezweb@gmail.com>
2023-12-08 14:36:26 +0100

    day 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import { server } from 'typescript';
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), svelte()],
+  output: "server"
 });
